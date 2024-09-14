709c3f7dbcf8e282eadd55043b1f350028209ab0
mark.wang <wxgter@gmail.com>
2024-09-14 22:37:34 +0800

    change the way data are loaded

--- polars_financial/test/test_metrics.py
+++ polars_financial/test/test_metrics.py
@@ -19,10 +19,10 @@ returns = {
 
 
 cum_return_test_data = [
-    (returns["empty"], []),
-    (returns["none"], [0, 0]),
+    ("empty", []),
+    ("none", [0, 0]),
     (
-        returns["mixed-nan"],
+        "mixed-nan",
         [
             0,
             0.01,
@@ -36,7 +36,7 @@ cum_return_test_data = [
         ],
     ),
     (
-        returns["mixed-none"],
+        "mixed-none",
         [
             0,
             0.01,
@@ -50,7 +50,7 @@ cum_return_test_data = [
         ],
     ),
     (
-        returns["negative"],
+        "negative",
         [
             0.0,
             -0.06,
@@ -66,64 +66,68 @@ cum_return_test_data = [
 ]
 
 cum_return_final_test_data = [
-    # (returns["empty"], None),
-    (returns["none"], 0),
-    (returns["mixed-nan"], 0.0389311),
-    (returns["mixed-none"], 0.038931),
-    (returns["negative"], -0.3659073),
+    # ("empty", None),
+    ("none", 0),
+    ("mixed-nan", 0.0389311),
+    ("mixed-none", 0.038931),
+    ("negative", -0.3659073),
 ]
 
 
 max_drawdown_test_data = [
-    # (returns["empty"], None),
-    (returns["none"], 0),
-    (returns["one-return"], 0),
-    (returns["mixed-nan"], -0.1),
-    (returns["positive"], 0),
-    (returns["negative"], -0.365907),
+    # ("empty", None),
+    ("none", 0),
+    ("one-return", 0),
+    ("mixed-nan", -0.1),
+    ("positive", 0),
+    ("negative", -0.365907),
 ]
 
 ann_return_test_data = [
-    (returns["mixed-nan"], DAILY, 1.9135925373194231),
-    (returns["for-annual"], WEEKLY, 0.24690830513998208),
-    (returns["for-annual"], MONTHLY, 0.052242061386048144),
+    ("mixed-nan", DAILY, 1.9135925373194231),
+    ("for-annual", WEEKLY, 0.24690830513998208),
+    ("for-annual", MONTHLY, 0.052242061386048144),
 ]
 
 ann_volatility_test_data = [
-    (returns["flat-line"], DAILY, 0.0),
-    (returns["mixed-nan"], DAILY, 0.9136465399704637),
-    (returns["for-annual"], WEEKLY, 0.38851569394870583),
-    (returns["for-annual"], MONTHLY, 0.18663690238892558),
+    ("flat-line", DAILY, 0.0),
+    ("mixed-nan", DAILY, 0.9136465399704637),
+    ("for-annual", WEEKLY, 0.38851569394870583),
+    ("for-annual", MONTHLY, 0.18663690238892558),
 ]
 
 calmar_ratio_test_data = [
-    (returns["flat-line"], DAILY, None),  # TODO: check if this is actually None?
-    (returns["one-return"], DAILY, None),  # TODO: check if this is actually None?
-    (returns["mixed-nan"], DAILY, 19.135925373194233),
-    (returns["for-annual"], WEEKLY, 2.4690830513998208),
-    (returns["for-annual"], MONTHLY, 0.52242061386048144),
+    ("flat-line", DAILY, None),  # TODO: check if this is actually None?
+    ("one-return", DAILY, None),  # TODO: check if this is actually None?
+    ("mixed-nan", DAILY, 19.135925373194233),
+    ("for-annual", WEEKLY, 2.4690830513998208),
+    ("for-annual", MONTHLY, 0.52242061386048144),
 ]
 
 sharpe_ratio_test_data = [
-    (returns["empty"], 0.0, None),
-    (returns["none"], 0.0, None),
-    (returns["one-return"], 0.0, None),
-    (returns["mixed-nan"], pl.col("returns"), None),
-    (returns["mixed-nan"], 0.0, 1.7238613961706866),
-    (returns["positive"], 0.0, 52.915026221291804),
-    (returns["negative"], 0.0, -24.406808633910085),
-    (returns["flat-line"], 0.0, float("inf")),
-    # (returns["mixed-nan"], simple_benchmark, 0.34111411441060574),   # TODO: add additional tests?
+    ("empty", 0.0, None),
+    ("none", 0.0, None),
+    ("one-return", 0.0, None),
+    ("mixed-nan", "mixed-nan", None),
+    ("mixed-nan", 0.0, 1.7238613961),
+    ("positive", 0.0, 52.9150262212),
+    ("negative", 0.0, -24.406808633),
+    ("flat-line", 0.0, float("inf")),
+    (["mixed-nan", "simple-benchmark"], "simple-benchmark", 0.3411141144),
 ]
 
 
-def _test_single_value(input, expected, method, *args, **kwargs):
-    schema = {"returns": pl.Float64}
+def _test_single_value(data, input, expected, method, *args, **kwargs):
 
-    metric = getattr(pl.col("returns").metrics, method)
-    df_output = pl.DataFrame({"returns": input}, schema=schema).select(
-        metric(*args, **kwargs)
-    )
+    if isinstance(input, list):
+        schema = {inp: pl.Float64 for inp in input}
+        data_dict = {inp: data[inp] for inp in input}
+    else:
+        schema = {input: pl.Float64}
+        data_dict = {input: data[input]}
+
+    metric = getattr(pl.col(input).metrics, method)
+    df_output = pl.DataFrame(data_dict, schema=schema).select(metric(*args, **kwargs))
 
     if expected is None:
         assert df_output[0, 0] is None
@@ -133,32 +137,36 @@ def _test_single_value(input, expected, method, *args, **kwargs):
 
 @pytest.mark.parametrize("input, expected", max_drawdown_test_data)
 def test_max_drawdown(input, expected):
-    _test_single_value(input, expected, "max_drawdown")
+    _test_single_value(returns, input, expected, "max_drawdown")
 
 
 @pytest.mark.parametrize("input, expected", cum_return_final_test_data)
 def test_cum_return_final(input, expected):
-    _test_single_value(input, expected, "cum_return_final")
+    _test_single_value(returns, input, expected, "cum_return_final")
 
 
 @pytest.mark.parametrize("input, annual_obs, expected", ann_return_test_data)
 def test_ann_return(input, annual_obs, expected):
-    _test_single_value(input, expected, "ann_return", annual_obs=annual_obs)
+    _test_single_value(returns, input, expected, "ann_return", annual_obs=annual_obs)
 
 
 @pytest.mark.parametrize("input, annual_obs, expected", ann_volatility_test_data)
 def test_ann_volatility(input, annual_obs, expected):
-    _test_single_value(input, expected, "ann_volatility", annual_obs=annual_obs)
+    _test_single_value(
+        returns, input, expected, "ann_volatility", annual_obs=annual_obs
+    )
 
 
 @pytest.mark.parametrize("input, annual_obs, expected", calmar_ratio_test_data)
 def test_calmar_ratio(input, annual_obs, expected):
-    _test_single_value(input, expected, "calmar_ratio", annual_obs=annual_obs)
+    _test_single_value(returns, input, expected, "calmar_ratio", annual_obs=annual_obs)
 
 
 @pytest.mark.parametrize("input, risk_free, expected", sharpe_ratio_test_data)
 def test_ann_sharpe_ratio(input, risk_free, expected):
-    _test_single_value(input, expected, "ann_sharpe_ratio", risk_free=risk_free)
+    _test_single_value(
+        returns, input, expected, "ann_sharpe_ratio", risk_free=risk_free
+    )
 
 
 # TODO: input data as dictionary or dataframe? Add dataframe builder helper function
